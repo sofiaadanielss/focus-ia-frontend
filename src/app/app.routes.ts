ab6974a087bbaeb18d038f1a5e6c4d8a6e13410e
sofiaadanielss <dannasofdaniels@gmail.com>
2026-04-07 21:22:31 -0700

    agregando ruta del dashoboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Profile } from './pages/profile/profile';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'profile', component: Profile },
+  { path: 'dashboard', component: Dashboard },
 ];
 
