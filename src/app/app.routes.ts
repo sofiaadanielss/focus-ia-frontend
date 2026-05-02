4e242cb7a186ea40473561951bb7407daed97e6f
sofiaadanielss <dannasofdaniels@gmail.com>
2026-05-02 00:42:03 -0700

    bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Login } from './pages/login/login';
 import { Profile } from './pages/profile/profile';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Adhd } from './pages/adhd-questions/adhd-questions';
+import { IqComponent } from './pages/iq/iq';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'register', component: Login },
   { path: 'adhd', component: Adhd },
   { path: 'profile', component: Profile },
-  { path: 'dashboard', component: Dashboard }
+  { path: 'dashboard', component: Dashboard },
+  { path: 'iq', component: IqComponent}
 ];
